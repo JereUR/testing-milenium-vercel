e16017befd1621d71c3c15814d9d4ddb04910afb
Leandro Libutti <leanlibutti@gmail.com>
2023-06-13 21:14:32 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cryptic-hollows-63599/', // Agrega esta línea con el nombre de tu aplicación
   plugins: [react()],
   server: {
     port: 3000,
